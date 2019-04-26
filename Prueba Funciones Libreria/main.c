35551dbc6e97e03f210716f1bd5b22c7755655c9
Mauro Bernabeu Veron <mauu.bernabeu1994@gmail.com>
2019-04-26 13:19:04 -0300

    añadi el proyecto de prueba de funciones de librerias de c propias

---
+++ Prueba Funciones Libreria/main.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../my_string.h"
+
+int main()
+{
+    char numStr[45]="-6666";
+    int numInt=-4;
+    int base= 2;
+    char numStr2[45];
+ //   ingresar_num_txt(numStr);
+
+//    if(!es_numero(numStr))
+//    {
+//        puts("La cadena ingresada no es un numero");
+//        return 1;
+//    }
+
+    printf("%s\n\n",numStr);
+    printf("%d\n\n",mi_atoi(numStr));
+
+
+
+
+//    int num=mi_atoi(numStr);
+//    printf("El numero es %d\n,num");
+
+    printf("%d de base 10 a base %d = %s\n\n  ",numInt , base , mi_itoa(numInt,numStr2,base));
+
+    //printf("Entero: %d ---- String: %s",numInt, mi_itoa(numInt,numStr2));
+
+
+    ////////////////////////////////////////////////////////////////////////////////
+    //NORMALIZAR
+    char frase[]={"hOLA.-+*QUE/t  tAl?.;-+/"}
+
+    //HOLA Que Tal\0
+
+    normalizar_AyN(frase);
+
+
+    return 0;
+}
+
+
